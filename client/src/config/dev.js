f99cb217e174fe82c9e8d0500b56e35bc9879bd6
Baran <barandurak07@gmail.com>
2021-03-14 22:54:00 +0300

    gitignore fix

--- client/src/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    googleClientID: "216828814401-hl266uhqtcriees3s70doe5a8gp384r0.apps.googleusercontent.com",
-};
--- util/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-
-    mongoURI: "mongodb+srv://barandurak:46135246@cluster0.xf0ds.mongodb.net/commerceDB?retryWrites=true&w=majority",
-    googleClientID: "216828814401-hl266uhqtcriees3s70doe5a8gp384r0.apps.googleusercontent.com",
-    googleSecretID: "Zn2Y4wzACgsdhZUQMfP0FUgI",
-    jwtSecret: "baransecretajwhı465das"
-};
